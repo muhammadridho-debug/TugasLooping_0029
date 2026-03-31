659f08218a3efeb48dad02e63c0574b014940274
muhammadridho-debug <idomuhammad11@gmail.com>
2026-03-31 16:37:42 +0800

    Melengkapi isi prosedur tampilkanMenu

--- fibonacci.cpp
+++ fibonacci.cpp
@@ -5,7 +5,11 @@ int angka;
 int pilihan;
 
 void tampilkanMenu() {
-
+    cout << "\n=== PROGRAM PENGECEKAN BILANGAN ===" << endl;
+    cout << "1. Cek Bilangan Prima" << endl;
+    cout << "2. Cek Bilangan Fibonacci" << endl;
+    cout << "0. Keluar" << endl;
+    cout << "Masukkan pilihan Anda: ";
 }
 
 
